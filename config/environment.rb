1f55437810e93ec353a9d5ab1e3606c0c68975da
Ariel Herman <aeh.herman@gmail.com>
2017-10-05 08:51:51 -0400

    updated environment file to change ActiveRecord logger to remove junk notation

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ Bundler.require
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development')
 require_all 'lib'
 # require_all 'bin'
+
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
